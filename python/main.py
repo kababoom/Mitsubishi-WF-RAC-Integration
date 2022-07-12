b3da206dc302d4609b820372043e089379e4d9b6
Marco Hagen <marco.hagen@wtss.nl>
2022-07-12 22:05:06 +0200

    Initial version

--- python/main.py
+++ python/main.py
@@ -1,16 +1,57 @@
+from ast import For, operator
+from email.utils import parsedate_to_datetime
 import json
 from src.parser import Parser
 from src.models.aircon import Aircon
 from src.repository.repository import Repository
 from src.utils import Utils
 from base64 import b64decode
+import time
+import datetime
 
-woonkamer = Parser.translateBytes(Repository.getAirconStats(ip='192.168.178.207'))
+while True:
 
-woonkamer.WindDirectionUD = 2
+    print(str(datetime.datetime.now()) + " - " + "Start execution")
+    ipList = ['192.168.10.177', '192.168.10.185']
+    presetTemp = 20
+    sleepInterval = 900
 
-outputJson = Parser.toBase64(airconStat=Utils.convertAirconToAirconStat(woonkamer))
-response = Parser.translateBytes(Repository.sendAircoCommand(command=outputJson, ip='192.168.178.207'))
+    for ip in ipList:
+        try:
+            print()
+            print(str(datetime.datetime.now()) + " - " + ip + " connecting")
 
-convertedAircon = json.dumps(response.__dict__, sort_keys=False, indent=4)
-print(convertedAircon)
+            airconStatResponse = Repository.getAirconStats(ip=ip)
+            operatorId = airconStatResponse['remoteList'][0]
+            aircoUnit = Parser.translateBytes(airconStatResponse['airconStat'])
+
+            # print(json.dumps(aircoUnit.__dict__, sort_keys=False, indent=4))
+            if aircoUnit.Operation == True:
+                if aircoUnit.PresetTemp < presetTemp:
+                    # setting temp
+                    aircoUnit.PresetTemp = presetTemp
+
+                    outputJson = Parser.toBase64(
+                        airconStat=Utils.convertAirconToAirconStat(aircoUnit))
+
+                    response = Parser.translateBytes(Repository.sendAircoCommand(
+                        command=outputJson, ip=ip, operatorId=operatorId))
+                    print(str(datetime.datetime.now()) + " - " + ip + " preset temp set to: " +
+                          str(response.PresetTemp))
+
+                    # convertedAircon = json.dumps(response.__dict__, sort_keys=False, indent=4)
+                    # print(convertedAircon)
+                else:
+                    print(str(datetime.datetime.now()) + " - " +
+                          ip + " is running at correct preset temp")
+
+            else:
+                print(str(datetime.datetime.now()) +
+                      " - " + ip + " is not running")
+        except:
+            print(str(datetime.datetime.now()) + " - An exception occured")
+
+    print()
+    print(str(datetime.datetime.now()) + " - Sleeping for " +
+          str(sleepInterval) + " seconds")
+    time.sleep(sleepInterval)
--- python/src/repository/repository.py
+++ python/src/repository/repository.py
@@ -1,19 +1,27 @@
+from ast import Str
 from requests import post, Response
 import time
 
+
 class Repository:
+
     def getAirconStats(ip: str) -> str:
-        url = 'http://%s:51443/beaver/command/getAirconStat'%ip
-        myobj = {'apiVer':'1.0','command':'getAirconStat','deviceId':'1234567890ABCDEF','operatorId':'d2bc4571-1cea-4858-b0f2-34c18bef1901','timestamp':1649703587}
+        url = 'http://%s:51443/beaver/command/getAirconStat' % ip
+        myobj = {'apiVer': '1.0', 'command': 'getAirconStat', 'deviceId': '1234567890ABCDEF',
+                 'operatorId': '8AE05915-C0B5-45FE-A053-B256B408F041', 'timestamp': 1649703587}
 
         x = post(url, json=myobj)
+        # print(x.json()['contents'])
+        return x.json()['contents']
 
-        return x.json()['contents']['airconStat']
-    
-    def sendAircoCommand( command:str, ip: str):
-        url = 'http://%s:51443/beaver/command/setAirconStat'%ip
-        myobj = {'apiVer':'1.0','command':'setAirconStat','contents': { 'airconId':'a043b05ad1f2', 'airconStat':command },'deviceId':'a68d811862d2ef38','operatorId':'54302674-f763-4deb-bdba-46d8d92c152d','timestamp': round(time.time())}
+    def sendAircoCommand(command: str, ip: str, operatorId: str):
+        url = 'http://%s:51443/beaver/command/setAirconStat' % ip
+        myobj = {'apiVer': '1.0', 'command': 'setAirconStat', 'contents': {'airconId': 'ec0bae9f398e', 'airconStat': command},
+                 'deviceId': 'a68d811862d2ef38', 'operatorId': operatorId, 'timestamp': round(time.time())}
 
         x = post(url, json=myobj)
+        # print(x.json())
 
         return x.json()['contents']['airconStat']
+
+# b'{"command":"setAirconStat","apiVer":"1.0","operatorId":"d2bc4571-1cea-4858-b0f2-34c18bef1901","deviceId":"a68d811862d2ef38","timestamp":1657622836,"result":2,"contents":{"airconId":"ec0bae9f398e","airconStat":"AAeimqr/AAAAAAASCgAAAAAAAf/////DVIAECBIkkwAAiAAAAgAAAAAAAAOAIJP/gBDS/5QQAwCbsQ==","logStat":0,"updatedBy":"aircon","expires":1657616549,"ledStat":1,"autoHeating":0,"highTemp":"AB","lowTemp":"66","firmType":"WF-RAC","wireless":{"firmVer":"010"},"mcu":{"firmVer":"123"},"timezone":"Europe/Amsterdam","remoteList":["8AE05915-C0B5-45FE-A053-B256B408F041","","",""],"numOfAccount":1}}'
